276f796143c151f0f8b9c989bd7498ce42f2963c
aditya <adithyachalla8@gmail.com>
2024-05-24 04:00:51 +0530

    Initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import Sidebar from "../sidebar/Sidebar";
-import "./navbar.scss";
 import { motion } from "framer-motion";
+
+import "./navbar.scss";
+import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
